3f34259c7f265949c4f2ae51581f59f1ca77b44e
Zonin <business.kariukimartin@gmail.com>
2023-09-21 17:30:53 +0300

    initial commit

---
+++ hello.py
@@ -0,0 +1,22 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return "<h1>Hello World</h1>"
+
+
+@app.route("/moji")
+def moji():
+    return "Moji shorti Baba is "
+
+
+@app.route("/hello/<name>")
+def hello_name(name):
+    return "Life will be okay %s!" % name
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
